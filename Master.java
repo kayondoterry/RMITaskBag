215389a96f0d41d3afdb12adadf3c58f7d530177
kayondoterry <kayondoterry@gmail.com>
2021-11-04 08:33:04 +0300

    added comments

--- Master.java
+++ Master.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
+// used for json serializing and deserializing
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,10 +15,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Master {
 
+  // host machine of the TaskBag
   private String hostname = "localhost";
   private String taskBagName = "TaskBag";
+  // holds results of computations by workers
   private Map<String, List<Integer>> results;
-  private int N, subTaskSize;
+  // prime numbers between 1 and N
+  // subTaskSize is the range of numbers for each subtask processed by a worker
+  private int N, subTaskSize; /
 
   public Master(String taskBagHostname, int N, int subTaskSize) {
     this.results = new TreeMap<>();
@@ -28,6 +33,7 @@ public class Master {
 
   public static void main(String[] args) {
 
+    // get hostname command-line argument
     String host = "";
     try{
       host = args[0];
@@ -38,6 +44,7 @@ public class Master {
 
     Scanner in = new Scanner(System.in);
 
+    // get required information from users
     System.out.println("Prime numbers between 1 and N.");
     System.out.println("Enter N: ");
     int N= in.nextInt();
@@ -51,29 +58,37 @@ public class Master {
   }
 
   public void run() {
-
+    // break Task into subtasks
     List<List<Integer>> subTasks = generateSubTasks(N, subTaskSize);
+    // subTasks encoded as JSON String
+    // Strings can be sent to remote objects because they implement the Serializable interface
     String subTaskString = generateSubTaskString(subTasks);
 
     try {
+      // connect to rmiregistry of TaskBag host machine
       Registry registry = LocateRegistry.getRegistry(hostname); // TODO getRegistry()
       TaskBagRemote taskBagRemote = (TaskBagRemote) registry.lookup(taskBagName);
 
+      // add sub tasks to the remote TaskBag
       taskBagRemote.placePair("Task", subTaskString);
 
+      // used for progress updates
       int totalSubTasks = subTasks.size();
 
       System.out.println("Master started\nwaiting for workers.......\n");
 
+      // keep checking TaskBag for completed work by Workers
       while (subTasks.size() > 0) {
+        // iterate through the subtasks and check it their results are in the TaskBag
         for (List<Integer> range : new ArrayList<>(subTasks)) {
           String resultId = getResultId(range);
           String resultString = taskBagRemote.removePair(resultId);
 
-          if (resultString != null) {
-            subTasks.remove(range);
-            List<Integer> result = getResult(resultString);
-            results.put(resultId, result);
+          if (resultString != null) {// result retrieved
+            subTasks.remove(range); // remove subtask from list of those to be checked
+            List<Integer> result = getResult(resultString); // decode result into List of primes
+            results.put(resultId, result); // store the results
+            // progress update
             System.out.println("Got result " + results.size() + "/" + totalSubTasks + ": " + resultId);
           }
         }
@@ -83,9 +98,9 @@ public class Master {
       System.err.println("Master exception:");
       e.printStackTrace();
     }
-
+    
     System.out.println("The prime numbers between 1 and " + N + " are:\n");
-
+    // print the results
     for (Map.Entry<String, List<Integer>> entry : results.entrySet()) {
       List<Integer> primes = entry.getValue();
       for (Integer prime : primes) {
@@ -95,6 +110,7 @@ public class Master {
 
   }
 
+  // breaks a Task into subtasks
   private List<List<Integer>> generateSubTasks(int N, int subTaskSize) {
     int totalSubTasks = N / subTaskSize;
     int remainder = N % subTaskSize;
@@ -117,6 +133,7 @@ public class Master {
 
   }
 
+  // encodes subtasks as JSON string
   private String generateSubTaskString(List<List<Integer>> subTasks) {
     ObjectMapper objectMapper = new ObjectMapper();
     try {
@@ -128,6 +145,7 @@ public class Master {
 
   }
 
+  // decode list of primes from JSON string
   private List<Integer> getResult(String resultString) {
     ObjectMapper objectMapper = new ObjectMapper();
     try {
@@ -139,6 +157,7 @@ public class Master {
     }
   }
 
+  // derive subtask/result id based on the range of numbers to be checked for primes
   private String getResultId(List<Integer> range) {
     return range.get(0) + "-" + range.get(1);
   }
