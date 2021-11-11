56a8c562d6912c1a0ffda7dd8ba2dff16942bbc6
kayondoterry <kayondoterry@gmail.com>
2021-11-11 03:00:35 +0300

    added comments to TaskBagRemote Interface

--- TaskBagRemote.java
+++ TaskBagRemote.java
@@ -2,7 +2,13 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface TaskBagRemote extends Remote{
+  
+  // adds a Pair() to the task bag
   void placePair(String key, String value) throws RemoteException;
+
+  // gets a Pair() with the key, without removing it from the task bag
   String takePair(String key) throws RemoteException;
+
+  // gets a pair with the key and removes the pair from the task bag
   String removePair(String key) throws RemoteException;
 }
